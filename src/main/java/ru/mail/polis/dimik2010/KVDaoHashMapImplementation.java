888b95ef0859705bb81afb74c18301f59c8e163f
Dmitry Zolotukhin <dimik2010@gmail.com>
2018-04-02 16:29:41 +0300

    Changed key in Map to from Integer to ByteBuffer

--- src/main/java/ru/mail/polis/dimik2010/KVDaoHashMapImplementation.java
+++ src/main/java/ru/mail/polis/dimik2010/KVDaoHashMapImplementation.java
@@ -4,31 +4,32 @@ import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.KVDao;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class KVDaoHashMapImplementation implements KVDao {
-    private final Map<Integer, byte[]> data = new HashMap<>();
+    private final Map<ByteBuffer, byte[]> data = new HashMap<>();
 
     @NotNull
     @Override
     public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
-        if (!data.containsKey(Arrays.hashCode(key))) {
+        if (!data.containsKey(ByteBuffer.wrap(key))) {
             throw new NoSuchElementException("No such element with key = " + Arrays.toString(key));
         }
-        return data.get(Arrays.hashCode(key));
+        return data.get(ByteBuffer.wrap(key));
     }
 
     @Override
     public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
-        data.put(Arrays.hashCode(key), value);
+        data.put(ByteBuffer.wrap(key), value);
     }
 
     @Override
     public void remove(@NotNull byte[] key) throws IOException {
-        data.remove(Arrays.hashCode(key));
+        data.remove(ByteBuffer.wrap(key));
     }
 
     @Override
