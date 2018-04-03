3a7a09281b478579e8e570b5fc07b1290dc802f8
aleksey kotelevskiy <a_kotelevskiy@mail.ru>
2018-04-03 17:50:16 +0300

    ByteWrap key

--- src/main/java/ru/mail/polis/alexeykotelevskiy/KVDaoImpl.java
+++ src/main/java/ru/mail/polis/alexeykotelevskiy/KVDaoImpl.java
@@ -1,6 +1,7 @@
 package ru.mail.polis.alexeykotelevskiy;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.NoSuchElementException;
 
@@ -9,25 +10,25 @@ import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.KVDao;
 
 public class KVDaoImpl implements KVDao {
-    private HashMap<String, byte[]> hashMap = new HashMap<>();
+    private HashMap<ByteBuffer, byte[]> hashMap = new HashMap<>();
 
     @NotNull
     @Override
     public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
-        String strKey= new String(key);
-        if (!hashMap.containsKey(strKey)) throw new NoSuchElementException();
-        return hashMap.get(strKey);
+        ByteBuffer bKey = ByteBuffer.wrap(key);
+        if (!hashMap.containsKey(bKey)) throw new NoSuchElementException();
+        return hashMap.get(bKey);
     }
 
     @Override
     public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
 
-        hashMap.put(new String(key), value);
+        hashMap.put(ByteBuffer.wrap(key), value);
     }
 
     @Override
     public void remove(@NotNull byte[] key) throws IOException {
-        hashMap.remove(new String(key));;
+        hashMap.remove(ByteBuffer.wrap(key));
     }
 
     @Override
