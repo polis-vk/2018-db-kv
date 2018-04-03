03483ce35a0c0cca42f2c01722270c378b504b85
Александр Тарасов <tarasovae97@mail.ru>
2018-04-03 19:53:38 +0300

    change to normal string

--- src/main/java/ru/mail/polis/ATarasov97/KVService.java
+++ src/main/java/ru/mail/polis/ATarasov97/KVService.java
@@ -2,11 +2,13 @@ package ru.mail.polis.ATarasov97;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 import org.jetbrains.annotations.NotNull;
+
 
 import ru.mail.polis.KVDao;
 
@@ -14,24 +16,31 @@ public class KVService implements KVDao {
 
     private final Map<String, byte[]> data = new HashMap<>();
 
+    private String wrapKey(byte[] key) {
+        StringBuilder builder = new StringBuilder();
+        for (byte value : key) {
+            builder.append(value);
+        }
+        return builder.toString();
+    }
+
     @NotNull
     @Override
     public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
-        if (!data.containsKey(Arrays.toString(key))) {
+        if (!data.containsKey(wrapKey(key))) {
             throw new NoSuchElementException("No element with key:" + Arrays.toString(key));
         }
-        return data.get(Arrays.toString(key));
+        return data.get(wrapKey(key));
     }
 
     @Override
     public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
-        data.put(Arrays.toString(key), value);
-        String a = "aaa";
+        data.put(wrapKey(key), value);
     }
 
     @Override
     public void remove(@NotNull byte[] key) throws IOException {
-        data.remove(Arrays.toString(key));
+        data.remove(wrapKey(key));
     }
 
     @Override
