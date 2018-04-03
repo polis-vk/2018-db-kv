81a16c0e360b88a341c1979eef811c2dd91d6782
Second#2Chance! <zheny.tim@gmail.com>
2018-04-03 23:14:22 +0300

    kvdao impl

--- src/main/java/ru/mail/polis/zhenytim/KVDaoImpl.java
+++ src/main/java/ru/mail/polis/zhenytim/KVDaoImpl.java
@@ -1,4 +1,34 @@
 package ru.mail.polis.zhenytim;
 
-public class KVDaoImpl {
+import org.jetbrains.annotations.NotNull;
+import ru.mail.polis.KVDao;
+
+import java.io.IOException;
+import java.util.*;
+
+public class KVDaoImpl implements KVDao {
+
+    private final Map<String, byte[]> data = new TreeMap<>();
+
+    @NotNull
+    @Override
+    public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
+        if(!data.containsKey(new String(key))) throw new NoSuchElementException();
+        return data.get(new String(key));
+    }
+
+    @Override
+    public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
+        data.put(new String(key), value);
+    }
+
+    @Override
+    public void remove(@NotNull byte[] key) throws IOException {
+        data.remove(new String(key));
+    }
+
+    @Override
+    public void close() throws IOException {
+
+    }
 }
