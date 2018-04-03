8f6402f64c7fc61f66c207b70a67e0310f5f1a6a
Daniil Sudzhaev <sudzhaev.da@gmail.com>
2018-04-03 21:11:15 +0300

    changed byte[] serialization

--- src/main/java/ru/mail/polis/sudzhaev/KVDaoImpl.java
+++ src/main/java/ru/mail/polis/sudzhaev/KVDaoImpl.java
@@ -3,39 +3,36 @@ package ru.mail.polis.sudzhaev;
 import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.KVDao;
 
+import java.math.BigInteger;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class KVDaoImpl implements KVDao {
 
-    private final Map<String, byte[]> map = new HashMap<>();
+    private final Map<BigInteger, byte[]> map = new HashMap<>();
 
-    private String serializeKey(byte[] key) {
-        StringBuilder builder = new StringBuilder();
-        for (byte b : key) {
-            builder.append(b);
-        }
-        return builder.toString();
+    private BigInteger serializeKey(byte[] key) {
+        return new BigInteger(key);
     }
 
     @NotNull
     @Override
     public byte[] get(@NotNull byte[] key) throws NoSuchElementException {
-        String strKey = serializeKey(key);
+        BigInteger strKey = serializeKey(key);
         if (!map.containsKey(strKey)) throw new NoSuchElementException();
         return map.get(strKey);
     }
 
     @Override
     public void upsert(@NotNull byte[] key, @NotNull byte[] value) {
-        String strKey = serializeKey(key);
+        BigInteger strKey = serializeKey(key);
         map.put(strKey, value);
     }
 
     @Override
     public void remove(@NotNull byte[] key) {
-        String strKey = serializeKey(key);
+        BigInteger strKey = serializeKey(key);
         map.remove(strKey);
     }
 
