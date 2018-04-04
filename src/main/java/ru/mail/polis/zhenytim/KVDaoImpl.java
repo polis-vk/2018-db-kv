327e6585a5f64289d1be7224adfa59b035970b43
Second#2Chance! <zheny.tim@gmail.com>
2018-04-04 23:36:33 +0300

    bytebuffer

--- src/main/java/ru/mail/polis/zhenytim/KVDaoImpl.java
+++ src/main/java/ru/mail/polis/zhenytim/KVDaoImpl.java
@@ -4,40 +4,31 @@ import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.KVDao;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class KVDaoImpl implements KVDao {
 
-    private final Map<String, byte[]> data = new HashMap<>();
-
-    public static String bytesToString(byte[] bytes) {
-        char[] buffer = new char[bytes.length >> 1];
-        for (int i = 0; i < buffer.length; i++) {
-            int bpos = i << 1;
-            char c = (char) (((bytes[bpos] & 0x00FF) << 8) + (bytes[bpos + 1] & 0x00FF));
-            buffer[i] = c;
-        }
-        return new String(buffer);
-    }
+    private final Map<ByteBuffer, byte[]> data = new HashMap<>();
 
     @NotNull
     @Override
     public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
-        final byte[] res = data.get(bytesToString(key));
+        final byte[] res = data.get(ByteBuffer.wrap(key));
         if(res == null) throw new NoSuchElementException();
         return res;
     }
 
     @Override
     public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
-        data.put(bytesToString(key), value);
+        data.put(ByteBuffer.wrap(key), value);
     }
 
     @Override
     public void remove(@NotNull byte[] key) throws IOException {
-        data.remove(bytesToString(key));
+        data.remove(ByteBuffer.wrap(key));
     }
 
     @Override
