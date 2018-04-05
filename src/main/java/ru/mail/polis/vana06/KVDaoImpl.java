0817d39b6512d3b0db93d3c9ddcc1d6a0fdcb035
Ivan Kylchik <cilcic.v22@gmail.com>
2018-04-05 22:33:11 +0300

    Реализация интерфейса KVDao
    Проходит все тесты кроме nonUnicode. Выбивает ошибку на строке "Assert.assertArrayEquals(new String(a1).getBytes(), new String(a2).getBytes());". Возможно я неверно понял задание, а может это баг

---
+++ src/main/java/ru/mail/polis/vana06/KVDaoImpl.java
@@ -0,0 +1,39 @@
+package ru.mail.polis.vana06;
+
+import org.jetbrains.annotations.NotNull;
+import ru.mail.polis.KVDao;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+public class KVDaoImpl implements KVDao {
+
+    private final Map<ByteArrayWrapper, byte[]> map = new HashMap<>();
+
+    @NotNull
+    @Override
+    public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
+        ByteArrayWrapper byteKey = new ByteArrayWrapper(key);
+        if(!map.containsKey(byteKey)){
+            throw new NoSuchElementException();
+        }
+        return map.get(byteKey);
+    }
+
+    @Override
+    public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
+        map.put(new ByteArrayWrapper(key), value);
+    }
+
+    @Override
+    public void remove(@NotNull byte[] key) throws IOException {
+        map.remove(new ByteArrayWrapper(key));
+    }
+
+    @Override
+    public void close() throws IOException {
+        map.clear();
+    }
+}
