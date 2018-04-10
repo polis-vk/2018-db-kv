dfb18a042950b8cff507113c3f876ea4238459f8
Kirill Yarulin <kirillyarulin@mail.ru>
2018-04-10 18:38:51 +0300

    fix comments

--- src/main/java/ru/mail/polis/kirillyarulin/KVDaoImpl.java
+++ src/main/java/ru/mail/polis/kirillyarulin/KVDaoImpl.java
@@ -4,32 +4,34 @@ import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.KVDao;
 
 import java.io.IOException;
-import java.util.Arrays;
+import java.nio.ByteBuffer;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.NoSuchElementException;
 
 public class KVDaoImpl implements KVDao {
-    private Map<ByteArrayWrapper, ByteArrayWrapper> storage = new HashMap<>();
+    private final Map<ByteBuffer, byte[]> storage = new HashMap<>();
 
     @NotNull
     @Override
     public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
-        if (storage.isEmpty() || !storage.containsKey(new ByteArrayWrapper(key))) {
+        byte[] value = storage.get(ByteBuffer.wrap(key));
+
+        if (value == null) {
             throw new NoSuchElementException();
         }
 
-        return storage.get(new ByteArrayWrapper(key)).getArray();
+        return value;
     }
 
     @Override
     public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
-        storage.put(new ByteArrayWrapper(key), new ByteArrayWrapper(value));
+        storage.put(ByteBuffer.wrap(key), value);
     }
 
     @Override
     public void remove(@NotNull byte[] key) throws IOException {
-        storage.remove(new ByteArrayWrapper(key));
+        storage.remove(ByteBuffer.wrap(key));
     }
 
     @Override
@@ -37,33 +39,4 @@ public class KVDaoImpl implements KVDao {
 
     }
 
-
-    private class ByteArrayWrapper {
-        private byte[] array;
-
-        public ByteArrayWrapper(byte[] array) {
-            this.array = array;
-        }
-
-        public byte[] getArray() {
-            return array;
-        }
-
-        public void setArray(byte[] arr) {
-            this.array = array;
-        }
-
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            ByteArrayWrapper that = (ByteArrayWrapper) o;
-            return Arrays.equals(array, that.array);
-        }
-
-        @Override
-        public int hashCode() {
-            return Arrays.hashCode(array);
-        }
-    }
 }
