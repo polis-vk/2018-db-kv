84b809a362e0ad53c87d413c5311bd7eb10e4366
gorbatov <gorbatov.eg@gmail.com>
2018-04-20 22:03:03 +0300

    Upsert method implemented

--- src/main/java/ru/mail/polis/KVDaoFactory.java
+++ src/main/java/ru/mail/polis/KVDaoFactory.java
@@ -17,13 +17,16 @@
 package ru.mail.polis;
 
 import org.jetbrains.annotations.NotNull;
+import org.omg.CosNaming.NamingContextPackage.NotFound;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.NoSuchElementException;
+import java.util.Queue;
 
 /**
  * Custom {@link KVDao} factory
@@ -60,14 +63,34 @@ public final class KVDaoFactory {
         return new KVDaoImpl(data);
     }
 
-    private class StoredItem {
+    private static class StoredValue {
+        private byte[] bytes;
+        final private String container;
+
+        public StoredValue(byte[] bytes, String container) {
+            this.bytes = bytes;
+            this.container = container;
+        }
+
+        public byte[] getBytes() {
+            return bytes;
+        }
+
+        public void setBytes(byte[] bytes) {
+            this.bytes = bytes;
+        }
+
+        public String getContainer() {
+            return container;
+        }
+
         //implement me
     }
 
     private static class KVDaoImpl implements KVDao {
         final private String dirWithSep;
-
-        private final Map<ByteBuffer, StoredItem> storage;
+        private final Map<ByteBuffer, StoredValue> storage;
+        private final Queue<String> filesQueue = new LinkedList<>();
 
         public KVDaoImpl(File dir) {
             dirWithSep = dir + File.separator;
@@ -77,15 +100,33 @@ public final class KVDaoFactory {
         @NotNull
         @Override
         public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
-            final StoredItem storedValue = this.storage.get(ByteBuffer.wrap(key));
+            final StoredValue storedValue = this.storage.get(ByteBuffer.wrap(key));
             if (storedValue == null) throw new NoSuchElementException();
-            return storedValue;
+            return storedValue.getBytes();
         }
 
         @Override
         public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
-            this.storage.put(ByteBuffer.wrap(key), value);
-
+            StoredValue item = this.storage.get(ByteBuffer.wrap(key));
+            if (item == null) {
+                //creating new item and putting into container
+                try {
+                    this.storage.put(ByteBuffer.wrap(key), new StoredValue(value, createAndPut(key, value)));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+            } else {
+                //update value in storage and container
+                try {
+                    update(item.getContainer(), key, value);
+                    item.setBytes(value);
+                    this.storage.put(ByteBuffer.wrap(key), item);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    System.exit(1);
+                }
+            }
         }
 
         @Override
@@ -97,6 +138,19 @@ public final class KVDaoFactory {
 
         @Override
         public void close() throws IOException {
+
+        }
+
+        private String createAndPut(byte[] key, byte[] value) throws IOException {
+            long timeStamp = System.currentTimeMillis();
+            return Long.toString(timeStamp);
+        }
+
+        private void update(String container, byte[] key, byte[] value) throws IOException {
+
+        }
+
+        private void delete(String container, byte[] key) throws IOException {
 
         }
     }
