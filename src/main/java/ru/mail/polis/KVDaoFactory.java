319ce487ce4c8bb8321802f80dcf3aa358c6b944
gorbatov <gorbatov.eg@gmail.com>
2018-04-21 05:03:41 +0300

    Working on "Last unit does not have enough valid bits" exception in base64 decoder

--- src/main/java/ru/mail/polis/KVDaoFactory.java
+++ src/main/java/ru/mail/polis/KVDaoFactory.java
@@ -20,13 +20,30 @@ import org.jetbrains.annotations.NotNull;
 import org.omg.CosNaming.NamingContextPackage.NotFound;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.nio.ByteBuffer;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Arrays;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Map;
 import java.util.NoSuchElementException;
 import java.util.Queue;
+
+import javax.xml.bind.DatatypeConverter;
+
+import com.sun.javafx.util.Utils;
+
+import jdk.internal.util.xml.impl.Input;
+import sun.misc.BASE64Decoder;
 
 /**
  * Custom {@link KVDao} factory
@@ -60,14 +77,14 @@ public final class KVDaoFactory {
             throw new IllegalArgumentException("Path is not a directory: " + data);
         }
 
-        return new KVDaoImpl(data);
+        return new KVDaoImpl(data, 500);
     }
 
     private static class StoredValue {
         private byte[] bytes;
-        final private String container;
+        final private File container;
 
-        public StoredValue(byte[] bytes, String container) {
+        public StoredValue(byte[] bytes, File container) {
             this.bytes = bytes;
             this.container = container;
         }
@@ -80,7 +97,7 @@ public final class KVDaoFactory {
             this.bytes = bytes;
         }
 
-        public String getContainer() {
+        public File getContainer() {
             return container;
         }
 
@@ -88,13 +105,65 @@ public final class KVDaoFactory {
     }
 
     private static class KVDaoImpl implements KVDao {
-        final private String dirWithSep;
-        private final Map<ByteBuffer, StoredValue> storage;
-        private final Queue<String> filesQueue = new LinkedList<>();
+        final private String KV_SPLITTER = "-";
+        final private String ENDLINE = "\n";
+        final private int KBYTE = 1024;
 
-        public KVDaoImpl(File dir) {
-            dirWithSep = dir + File.separator;
-            storage = new HashMap<>();
+        final private int TRASH_HOLD;
+        final private String STORAGE_DIR;
+        private final Map<ByteBuffer, StoredValue> storage;
+        private final Queue<File> filesQueue = new LinkedList<>();
+
+        /*
+        * Chunk size in kbytes
+        */
+        public KVDaoImpl(final File dir, final int chunkSize) {
+            this.STORAGE_DIR = dir + File.separator;
+            this.TRASH_HOLD = chunkSize * KBYTE;
+            this.storage = new HashMap<>();
+            try {
+                java.nio.file.Files.walkFileTree(
+                        dir.toPath(),
+                        new SimpleFileVisitor<Path>() {
+                            private void fetchData(@NotNull final Path file) throws IOException {
+                                if (!file.toFile().canRead()) return;
+                                InputStream inputStream = new FileInputStream(file.toFile());
+                                //reading stored chunk through container
+                                String chunk = "";
+                                byte[] buffer = new byte[TRASH_HOLD / 4];
+                                int len;
+                                while ((len = inputStream.read(buffer)) != -1) {
+                                    chunk += new String(buffer);
+//                                    for (int i = 0; i < len; i++)
+//                                        chunk += (char)buffer[i];
+                                }
+                                inputStream.close();
+                                String[] items = chunk.split(ENDLINE);
+                                for (String item :
+                                        items) {
+                                    String[] parts = item.split(KV_SPLITTER);
+                                    if (parts.length == 2) {
+                                        storage.put(ByteBuffer.wrap(Base64.getDecoder().decode(parts[0].getBytes())),
+                                                new StoredValue(Base64.getDecoder().decode(parts[1].getBytes()),
+                                                        file.toFile()));
+                                    }
+                                }
+                                if (chunk.length() < TRASH_HOLD) filesQueue.add(file.toFile());
+                            }
+
+                            @NotNull
+                            @Override
+                            public FileVisitResult visitFile(@NotNull final Path file, @NotNull final BasicFileAttributes attrs)
+                                    throws IOException {
+                                System.out.println(file.getFileName());
+                                fetchData(file);
+                                return FileVisitResult.CONTINUE;
+                            }
+                        });
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            };
         }
 
         @NotNull
@@ -142,17 +211,99 @@ public final class KVDaoFactory {
 
         }
 
-        private String create(byte[] key, byte[] value) throws IOException {
-            long timeStamp = System.currentTimeMillis();
-            return Long.toString(timeStamp);
+        private File create(byte[] key, byte[] value) throws IOException {
+            if (filesQueue.size() == 0) {
+                File container = new File(STORAGE_DIR + Long.toString(System.currentTimeMillis()));
+                container.createNewFile();
+                OutputStream outputStream = new FileOutputStream(container, false);
+                outputStream.write((Base64.getEncoder().encodeToString(key) + KV_SPLITTER + Base64.getEncoder().encodeToString(value) + ENDLINE).getBytes());
+                outputStream.flush(); outputStream.close();
+                if (container.length()  < TRASH_HOLD) this.filesQueue.add(container);
+                return container;
+            } else {
+                File container = this.filesQueue.remove();
+                OutputStream outputStream = new FileOutputStream(container, true);
+                outputStream.write((Base64.getEncoder().encodeToString(key) + KV_SPLITTER + Base64.getEncoder().encodeToString(value) + ENDLINE).getBytes());
+                outputStream.flush(); outputStream.close();
+                if (container.length() < TRASH_HOLD) this.filesQueue.add(container);
+                return container;
+            }
+
         }
 
-        private void update(String container, byte[] key, byte[] value) throws IOException {
-
+        private void update(File container, byte[] key, byte[] value) throws IOException {
+            InputStream inputStream = new FileInputStream(container);
+            //reading stored chunk through container
+            String chunk = "";
+            byte[] buffer = new byte[256];
+            int len;
+            while ((len = inputStream.read(buffer)) != -1) {
+//                for (int i = 0; i < len; i++)
+//                    chunk += (char)buffer[i];
+                chunk += new String(buffer);
+            }
+            inputStream.close();
+            OutputStream outputStream = new FileOutputStream(container, false);
+            String[] items = chunk.split(ENDLINE);
+            boolean updated = false;
+            for (String item :
+                    items) {
+                if (updated) {
+                    outputStream.write((item.getBytes() + ENDLINE).getBytes());
+                    continue;
+                }
+                String[] parts = item.split(KV_SPLITTER);
+                if (parts.length != 2) continue;
+                byte[] decodedKey = Base64.getDecoder().decode(parts[0].getBytes());
+                if (Arrays.equals(key, decodedKey)) {
+                    outputStream.write((parts[0].getBytes() + KV_SPLITTER + Base64.getEncoder().encode(value) + ENDLINE).getBytes());
+                    updated = true;
+                    continue;
+                } else {
+                    outputStream.write((item.getBytes() + ENDLINE).getBytes());
+                    continue;
+                }
+            }
+            outputStream.flush(); outputStream.close();
         }
 
-        private void delete(String container, byte[] key) throws IOException {
-
+        private void delete(File container, byte[] key) throws IOException {
+            InputStream inputStream = new FileInputStream(container);
+            //reading stored chunk through container
+            String chunk = "";
+            byte[] buffer = new byte[256];
+            int len;
+            while ((len = inputStream.read(buffer)) != -1) {
+//                for (int i = 0; i < len; i++)
+//                    chunk += (char)buffer[i];
+                chunk += new String(buffer);
+            }
+            inputStream.close();
+            boolean containerIsInQueue = false;
+            if (chunk.length() < TRASH_HOLD) containerIsInQueue = true;
+            OutputStream outputStream = new FileOutputStream(container, false);
+            String[] items = chunk.split(ENDLINE);
+            boolean deleted = false;
+            for (String item :
+                    items) {
+                if (deleted) {
+                    outputStream.write((item.getBytes() + ENDLINE).getBytes());
+                    continue;
+                }
+                String[] parts = item.split(KV_SPLITTER);
+                if (parts.length != 2) continue;
+                byte[] decodedKey = Base64.getEncoder().encode(parts[0].getBytes());
+                if (Arrays.equals(key, decodedKey)) {
+                    deleted = true;
+                    continue;
+                } else {
+                    outputStream.write((item.getBytes() + ENDLINE).getBytes());
+                    continue;
+                }
+            }
+            outputStream.flush(); outputStream.close();
+            if (!containerIsInQueue)
+                if (container.length() < TRASH_HOLD) filesQueue.add(container);
         }
     }
 }
