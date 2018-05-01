655ce942d5607c192d5d638d494d72af040468bb
Aleksey.Antufev <Aleksey.Antufev@billing.ru>
2018-05-01 12:11:48 +0300

    dz2 complete

---
+++ src/main/java/ru/mail/polis/alexantufiev/KVDaoImpl.java
@@ -0,0 +1,68 @@
+package ru.mail.polis.alexantufiev;
+
+import org.jetbrains.annotations.NotNull;
+import ru.mail.polis.KVDao;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+/**
+ * Implementation of KVDao.
+ *
+ * @author Aleksey Antufev
+ * @version 1.1.0
+ * @since 1.1.0 01.05.2018
+ */
+public class KVDaoImpl implements KVDao {
+
+    private File data;
+
+    public KVDaoImpl(File data) {
+        this.data = data;
+    }
+
+    @NotNull
+    @Override
+    public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
+        File tempFile = new File(data + "\\" + Arrays.toString(key));
+        //        if (tempFile.exists()) {
+        //            return Files.readAllBytes(tempFile.toPath());
+        //        } else {
+        //            throw new NoSuchElementException("File bot found");
+        //        }
+        if (!tempFile.exists()) {
+            throw new NoSuchElementException("File not found");
+        }
+        try (FileInputStream fileInputStream = new FileInputStream(tempFile)) {
+            byte[] bytes = new byte[fileInputStream.available()];
+            if (fileInputStream.read(bytes) < 0) {
+                throw new UnsupportedOperationException("Can not read value");
+            }
+            return bytes;
+        }
+    }
+
+    @Override
+    public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(new File(data + "\\" + Arrays.toString(key)))) {
+            fileOutputStream.write(value);
+        }
+    }
+
+    @Override
+    public void remove(@NotNull byte[] key) {
+        File tempFile = new File(data + "\\" + Arrays.toString(key));
+        if (!tempFile.delete()) {
+            throw new UnsupportedOperationException("Can not be delete");
+        }
+    }
+
+    @Override
+    public void close() {
+
+    }
+}
