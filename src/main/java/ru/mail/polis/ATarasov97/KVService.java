6cdb4b2bf309850036ace4ee13287a5836c9bef4
Александр Тарасов <tarasovae97@mail.ru>
2018-05-08 12:03:23 +0300

    homework 2.1

--- src/main/java/ru/mail/polis/ATarasov97/KVService.java
+++ src/main/java/ru/mail/polis/ATarasov97/KVService.java
@@ -2,79 +2,55 @@ package ru.mail.polis.ATarasov97;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.NoSuchElementException;
 
 import org.jetbrains.annotations.NotNull;
+import org.mapdb.DB;
+import org.mapdb.DBMaker;
+import org.mapdb.HTreeMap;
+import org.mapdb.Serializer;
 
-
-import jetbrains.exodus.ArrayByteIterable;
-import jetbrains.exodus.ByteBufferByteIterable;
-import jetbrains.exodus.ByteIterable;
-import jetbrains.exodus.env.Environment;
-import jetbrains.exodus.env.Environments;
-import jetbrains.exodus.env.Store;
-import jetbrains.exodus.env.StoreConfig;
-import jetbrains.exodus.env.Transaction;
-import jetbrains.exodus.env.TransactionalExecutable;
 import ru.mail.polis.KVDao;
 
 public class KVService implements KVDao {
 
-    private final Environment env;
-    private final Map<ByteBuffer, byte[]> data = new HashMap<>();
+    private final DB db;
+    private final HTreeMap<byte[], byte[]> store;
 
     public KVService(@NotNull final File data) {
-        env = Environments.newInstance(data);
+        File dbData = new File(data, "db");
+        this.db = DBMaker
+                .fileDB(dbData)
+                .fileMmapEnableIfSupported()
+                .fileMmapPreclearDisable()
+                .make();
+        this.store = db.hashMap(dbData.getName())
+                .keySerializer(Serializer.BYTE_ARRAY)
+                .valueSerializer(Serializer.BYTE_ARRAY)
+                .createOrOpen();
     }
-    private ByteIterable wrap(byte[] key) {
-        return new ArrayByteIterable(key);
-    }
-
     @NotNull
     @Override
     public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
-        final ByteIterable[] result = new ByteIterable[1];
-        env.executeInTransaction(new TransactionalExecutable() {
-            @Override
-            public void execute(@NotNull Transaction txn) {
-                final Store store = env.openStore("ServiceStore", StoreConfig.WITHOUT_DUPLICATES, txn);
-                result[0] = store.get(txn, wrap(key));
-            }
-        });
-        if (result[0] == null) {
-            throw new NoSuchElementException("No element with key:" + Arrays.toString(key));
+        byte[] bytes = store.get(key);
+        if (bytes == null) {
+            throw new NoSuchElementException();
         }
-        return result[0].getBytesUnsafe();
+        return bytes;
     }
 
     @Override
     public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
-        env.executeInTransaction(new TransactionalExecutable() {
-            @Override
-            public void execute(@NotNull Transaction txn) {
-                final Store store = env.openStore("ServiceStore", StoreConfig.WITHOUT_DUPLICATES, txn);
-                store.put(txn, wrap(key), wrap(value));
-            }
-        });
+        store.put(key, value);
     }
 
     @Override
     public void remove(@NotNull byte[] key) throws IOException {
-        env.executeInTransaction(new TransactionalExecutable() {
-            @Override
-            public void execute(@NotNull Transaction txn) {
-                final Store store = env.openStore("ServiceStore", StoreConfig.WITHOUT_DUPLICATES, txn);
-                store.delete(txn, wrap(key));
-            }
-        });
+        store.remove(key);
     }
 
     @Override
     public void close() throws IOException {
-        env.close();
+        db.close();
     }
 }
