65909c0b74fce560209a9073d358321bddfd14e2
nikita <kobalt31@mail.ru>
2018-05-16 22:11:22 +0300

    fix remarks

--- src/main/java/ru/mail/polis/K1ta/MyKVDao.java
+++ src/main/java/ru/mail/polis/K1ta/MyKVDao.java
@@ -4,7 +4,7 @@ import org.jetbrains.annotations.NotNull;
 import ru.mail.polis.KVDao;
 
 import java.io.*;
-import java.nio.ByteBuffer;
+import java.nio.file.Files;
 import java.util.Arrays;
 import java.util.NoSuchElementException;
 
@@ -18,48 +18,30 @@ public class MyKVDao implements KVDao {
     @NotNull
     @Override
     public byte[] get(@NotNull byte[] key) throws NoSuchElementException, IOException {
-        ByteBuffer _key = ByteBuffer.wrap(key);
-        File _file = new File(file + "//" + Arrays.toString(_key.array()));
+        File _file = createFile(key);
         if (!_file.exists()) {
             throw new NoSuchElementException();
         }
-        FileInputStream f = null;
-        try {
-            f = new FileInputStream(_file);
-            byte[] val = new byte[f.available()];
-            f.read(val);
-            return val;
-        } finally {
-            assert f != null;
-            f.close();
-        }
+        return Files.readAllBytes(_file.toPath());
     }
 
     @Override
     public void upsert(@NotNull byte[] key, @NotNull byte[] value) throws IOException {
-        ByteBuffer _key = ByteBuffer.wrap(key);
-        File _file = new File(file + "//" + Arrays.toString(_key.array()));
-        _file.createNewFile();
-        FileOutputStream f = null;
-        try {
-            f = new FileOutputStream(_file);
-            f.write(value);
-        } finally {
-            assert f != null;
-            f.close();
-        }
+        File _file = createFile(key);
+        Files.write(_file.toPath(), value);
     }
 
     @Override
     public void remove(@NotNull byte[] key) {
-        ByteBuffer _key = ByteBuffer.wrap(key);
-        File _file = new File(file + "//" + Arrays.toString(_key.array()));
-        if (_file.exists()) {
-            _file.delete();
-        }
+        File _file = createFile(key);
+        _file.delete();
     }
 
     @Override
     public void close() {
+    }
+
+    private File createFile(byte[] key) {
+        return new File(file, Arrays.toString(key));
     }
 }
